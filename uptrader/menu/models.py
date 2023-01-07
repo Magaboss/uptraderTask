52c8f25b2feaf708860e49d071dae8ad4804e226
Saygidmagomed <103044176+Magaboss@users.noreply.github.com>
2023-01-07 07:19:12 +0300

    Update models.py

--- uptrader/menu/models.py
+++ uptrader/menu/models.py
@@ -1,62 +1,44 @@
 from django.db import models
-from django.urls import reverse
 
 
-class BaseAbstractModel(models.Model):
-    is_visible = models.BooleanField(default=True, verbose_name='Видимость')
-    order = models.IntegerField(default=10, verbose_name='Расположение по порядку')
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+class Post (models.Model):
+    """
+ Посты - подкатегории основного меню.
+    """
+    title = models.CharField('title', max_length=255)
+    slug  = models.SlugField('slug', max_length= 255)
+    menu = models.ForeignKey (
+        'Menu',
+        on_delete=models.CASCADE,
+        blank=True,
+        related_name='posts'
+    )
+    parent  = models.ForeignKey (
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='children'
+    )
 
     class Meta:
-        abstract = True
-
-
-class Menu(BaseAbstractModel):
-    title = models.CharField(max_length=20, verbose_name='Название меню')
-    slug = models.SlugField(max_length=255, verbose_name='URL', null=True)
-    url = models.CharField(max_length=255, verbose_name='Named URL', blank=True)
-
-    class Meta:
-        verbose_name = 'Меню'
-        verbose_name_plural = 'Меню'
+        verbose_name = 'Item menu'
+        verbose_name_plural = 'Items menu'
 
     def __str__(self):
         return self.title
 
-    def get_full_path(self):
-        if self.url:
-            full_url = reverse(self.url)
-        else:
-            full_url = f'/{self.slug}/'
 
-        return full_url
-
-
-class MenuItem(BaseAbstractModel):
-    menu = models.ForeignKey(Menu, related_name='items', verbose_name='Меню',
-                             blank=True, null=True, on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='items',
-                               verbose_name='Родительский пункт родительского меню',
-                               on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name='Пункт меню')
-    url = models.CharField(max_length=255, blank=True, verbose_name='Ссылка')
-    named_url = models.CharField(max_length=255, verbose_name='Именованный URL', blank=True)
+class Menu(models.Model):
+    """
+    Модель в БД основного меню.
+    """
+    title = models.CharField('Menu title', max_length=255, unique=True)
+    slug = models.SlugField('Menu slug', max_length=255)
 
     class Meta:
-        verbose_name = 'Пункт меню'
-        verbose_name_plural = 'Пункты меню'
-        ordering = ('order', )
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu_more'
 
     def __str__(self):
-        return self.name
-
-    def get_url(self):
-        if self.named_url:
-            url = reverse(self.named_url)
-        elif self.url:
-            url = self.url
-        else:
-            url = '/'
-
-        return url
+        return self.title
